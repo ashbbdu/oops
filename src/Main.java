eeabecc73e027a26e955e78f821b6e76afb29265
ashbbdu <ashishsrivastava7007@gmail.com>
2025-05-20 23:23:26 +0530

    java class and object

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,50 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static class Employee {
+      String name;
+       int salary;
+
+        public void setName (String _name) {
+            this.name = _name;
         }
+
+        public void setSalary(int _salary) {
+            this.salary = _salary;
+        }
+
+        public void getSalary () {
+            System.out.println(this.salary);
+        }
+    }
+
+    public static  class Student {
+        String name;
+        int rollNumber;
+
+        public  void setDetails (String _name , int _rollNumber) {
+            this.name = _name;
+            this.rollNumber = _rollNumber;
+        }
+
+        public void displayDetails () {
+            System.out.println(this.name);
+            System.out.println(this.rollNumber);
+        }
+    }
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        e1.setName("Ashish");
+        e1.setSalary(100);
+       e1.getSalary();
+       e1.setSalary(1000);
+       e1.getSalary();
+
+
+       Student s1 = new Student();
+       s1.setDetails("Ashish" , 21);
+       s1.displayDetails();
     }
 }

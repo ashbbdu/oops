7c3727a6bdb240a6818a20ac4a58928806544330
ashbbdu <ashishsrivastava7007@gmail.com>
2025-05-21 15:38:32 +0530

    constructor

---
+++ src/Bank.java
@@ -0,0 +1,14 @@
+class BankAccount {
+    private double balance;
+    private String name;
+
+    public BankAccount (double _balance , String name) {
+
+    }
+}
+
+public class Bank {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,37 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+class Employee {
+    public String name;
+    private int salary;
+
+
+//    constructor
+    public Employee (String _name , int _salary) {
+        this.name = _name;
+        this.salary = _salary;
+    }
+
+    public void setName (String _name) {
+        this.name = _name;
+    }
+
+    public void getName () {
+        System.out.println(this.name);
+    }
+
+    public void setSalary(int _salary) {
+        this.salary = _salary;
+    }
+
+    public void getSalary () {
+        System.out.println(this.salary);
+    }
+}
+
 public class Main {
 
-    public static class Employee {
-      String name;
-       int salary;
 
-        public void setName (String _name) {
-            this.name = _name;
-        }
-
-        public void setSalary(int _salary) {
-            this.salary = _salary;
-        }
-
-        public void getSalary () {
-            System.out.println(this.salary);
-        }
-    }
 
     public static  class Student {
         String name;
@@ -35,16 +49,20 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Employee e1 = new Employee();
-        e1.setName("Ashish");
-        e1.setSalary(100);
-       e1.getSalary();
-       e1.setSalary(1000);
-       e1.getSalary();
+        Employee e1 = new Employee("Madhur" , 300);
+        e1.getSalary();
+        e1.getName();
+//        e1.setName("Ashish");
+//        e1.setSalary(100);
+//       e1.getSalary();
+//       e1.setSalary(1000);
+//       e1.getSalary();
 
 
-       Student s1 = new Student();
-       s1.setDetails("Ashish" , 21);
-       s1.displayDetails();
+//       Student s1 = new Student();
+//       s1.setDetails("Ashish" , 21);
+//       s1.displayDetails();
+//        System.out.println( s1.name);
+//        System.out.println(s1.rollNumber);
     }
 }

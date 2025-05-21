5d2b8f0613550ec6739e86a36f88429c66fdde5f
ashbbdu <ashishsrivastava7007@gmail.com>
2025-05-21 15:59:24 +0530

    withdrawn function

--- src/Bank.java
+++ src/Bank.java
@@ -2,13 +2,39 @@ class BankAccount {
     private double balance;
     private String name;
 
-    public BankAccount (double _balance , String name) {
-
+    public BankAccount (double _balance , String _name) {
+        this.balance = _balance;
+        this.name = _name;
     }
+
+    public void setBalance (double _amount) {
+        this.balance = this.balance + _amount;
+    }
+    public void getBalance () {
+        System.out.println(this.balance);
+    }
+
+    public boolean withdrawn (double _amount) {
+        if(balance < _amount) {
+            System.out.println("Insufficient Funds !");
+            return false;
+        }
+        balance = balance - +_amount;
+        return true;
+    }
+
 }
 
 public class Bank {
     public static void main(String[] args) {
+        BankAccount person1 = new BankAccount(1000000.00 , "Ashish");
+        BankAccount person2 = new BankAccount(290000.00 , "Madhur");
+        person1.getBalance();
+        person2.getBalance();
 
+        person2.setBalance(120.00);
+        person2.getBalance();
+        person2.withdrawn(20.00);
+        person2.getBalance();
     }
 }

abce277f62c6e96f01724294129f67c773a8ee7f
ashbbdu <ashishsrivastava7007@gmail.com>
2025-05-21 16:47:02 +0530

    Bankaccount Class

--- src/Bank.java
+++ src/Bank.java
@@ -2,12 +2,21 @@ class BankAccount {
     private double balance;
     private String name;
 
+
+    public BankAccount () {
+        this.balance = 0;
+        this.name = "";
+    }
+
     public BankAccount (double _balance , String _name) {
         this.balance = _balance;
         this.name = _name;
     }
 
     public void setBalance (double _amount) {
+        if(_amount <= 0) {
+            System.out.println("The amount should be greater than 0");
+        }
         this.balance = this.balance + _amount;
     }
     public void getBalance () {
@@ -34,7 +43,13 @@ public class Bank {
 
         person2.setBalance(120.00);
         person2.getBalance();
+
         person2.withdrawn(20.00);
+        person2.getBalance();
+
+        BankAccount p3 = new BankAccount();
+        p3.getBalance();
+        person2.setBalance(100);
         person2.getBalance();
     }
 }

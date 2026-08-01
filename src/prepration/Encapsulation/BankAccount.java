0b3b9af408e0c27d8abc1b66a899290d05ca56a8
ashbbdu <ashishsrivastava7007@gmail.com>
2026-08-01 12:39:13 +0530

    encapsulation

---
+++ src/prepration/Encapsulation/BankAccount.java
@@ -0,0 +1,24 @@
+package prepration.Encapsulation;
+
+
+
+public class BankAccount {
+    private String name;
+    private double balance;
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public void setBalance (double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance () {
+        return balance;
+    }
+}
---
+++ src/prepration/Encapsulation/Implementation.java
@@ -0,0 +1,10 @@
+package prepration.Encapsulation;
+
+public class Implementation {
+    public static void main(String[] args) {
+        BankAccount b1 = new BankAccount();
+
+        b1.setName("Ashish");
+        System.out.println(b1.getName());
+    }
+}
